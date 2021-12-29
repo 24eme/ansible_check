2791d7c2b0ebdede3b5eed0d0f10176d344a47d8
Tangui Morlier <tangui@tangui.eu.org>
2021-12-29 18:00:05 +0100

    On repasse par un fichier pivot pour éviter les effets de bord sur les synthèses

--- bin/ansible_check.sh
+++ bin/ansible_check.sh
@@ -15,7 +15,8 @@ for config in config/*inc ; do
 		html=$(echo $playbook | sed 's/\.yml/.html/')
 		export ANSIBLE_FORCE_COLOR=true
 		cp $actualdir/output_html/playbooks/$html $actualdir/output_html/playbooks/$html."previous"
-		$ANSIBLE_PLAYBOOK_CMD --ssh-extra-args "-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" --diff --check $playbook 2>&1 | aha -t "ansible playbook $playbook" > $actualdir/output_html/playbooks/$html
+		$ANSIBLE_PLAYBOOK_CMD --ssh-extra-args "-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" --diff --check $playbook 2>&1 | aha -t "ansible playbook $playbook" > $actualdir/output_html/playbooks/$html".running.htm"
+		mv $actualdir/output_html/playbooks/$html".running.htm" $actualdir/output_html/playbooks/$html
 	done
 	cd - 2> /dev/null
 
