22bc87ebb25f45bf805744242c1b0729cfb7a712
Tangui Morlier <tangui@tangui.eu.org>
2021-12-29 17:48:27 +0100

    Etourderie

--- bin/generate_html.sh
+++ bin/generate_html.sh
@@ -7,7 +7,7 @@ mkdir -p output_html/playbooks
 grep changed= output_html/playbooks/*html | grep ok= | sed 's/^[^:]*\/\([^:]*\).html:/\1@/' | sed 's/$/<\/p>/' | awk -F '@' 'BEGIN {print "<table>"} {print "<tr><td><a href=\"playbooks/"$1".html\">"$1"</a></td><td>"$2"</td></tr>"} END { print "</table>"}'  > output_html/index.html.tmp
 mv output_html/index.html.tmp output_html/index.html
 
-grep changed= output_html/playbooks/*html.previous | grep ok= | sed 's/^[^:]*\/\([^:]*\).html:/\1@/' | sed 's/$/<\/p>/' | awk -F '@' 'BEGIN {print "<table>"} {print "<tr><td><a href=\"playbooks/"$1".html\">"$1"</a></td><td>"$2"</td></tr>"} END { print "</table>"}'  > output_html/index.html.previous
+grep changed= output_html/playbooks/*html.previous | grep ok= | sed 's/^[^:]*\/\([^:]*\).html.previous:/\1@/' | sed 's/$/<\/p>/' | awk -F '@' 'BEGIN {print "<table>"} {print "<tr><td><a href=\"playbooks/"$1".html\">"$1"</a></td><td>"$2"</td></tr>"} END { print "</table>"}'  > output_html/index.html.previous
 
 echo "playbook;host;global color;ok;changed;unreachable;failed;skipped;rescued;ignored" > output_html/playbooks.csv
 cat output_html/index.html | grep tr | sed 's/style="color/style>/' | sed 's/;">/:/' | sed 's/<[^>]*>//g'  | sed 's/=/:/g' | sed 's/:/;/g' | sed 's/ *; */;/g'  | sed 's/  */;/g' | awk -F ';' '{print $1";"$3";"$2";"$5";"$7";"$9";"$11";"$13";"$15";"$17}' >> output_html/playbooks.csv
