dff40fbe7bc9ea71b532e910b623c91274da3165
Tangui Morlier <tangui@tangui.eu.org>
2021-12-29 19:14:46 +0100

    Tells if a ansible is running

--- bin/generate_html.sh
+++ bin/generate_html.sh
@@ -5,7 +5,12 @@ cd $(dirname $0)/.. 2> /dev/null
 mkdir -p output_html/playbooks
 
 grep changed= output_html/playbooks/*html | grep ok= | sed 's/^[^:]*\/\([^:]*\).html:/\1@/' | sed 's/$/<\/p>/' | awk -F '@' 'BEGIN {print "<table>"} {print "<tr><td><a href=\"playbooks/"$1".html\">"$1"</a></td><td>"$2"</td></tr>"} END { print "</table>"}'  > output_html/index.html.tmp
-echo -n "<p>Last update: " >> output_html/index.html.tmp ; ls -lrt --full-time  output_html/playbooks/ | tail -n  1 | awk '{print $6" "$7}' >> output_html/index.html.tmp ; echo "</p><p><a href='feed.xml'>RSS Feed</p>" >> output_html/index.html.tmp
+echo -n "<p>Last update: " >> output_html/index.html.tmp ; ls -lrt --full-time  output_html/playbooks/ | tail -n  1 | awk '{print $6" "$7}' >> output_html/index.html.tmp ; echo "</p>" >> output_html/index.html.tmp
+running=$(ls output_html/playbooks/*html.running.htm 2> /dev/null | sed 's/.*playbooks.//' | sed 's/.html.running.htm//')
+if test "$running"; then
+echo "<p><a href='playbooks/"$running".html.running.htm'>"$running" currently checked</a></p>" >> output_html/index.html.tmp
+fi
+echo "<p><a href='feed.xml'>RSS Feed</p>" >> output_html/index.html.tmp
 mv output_html/index.html.tmp output_html/index.html
 
 grep changed= output_html/playbooks/*html.previous | grep ok= | sed 's/^[^:]*\/\([^:]*\).html.previous:/\1@/' | sed 's/$/<\/p>/' | awk -F '@' 'BEGIN {print "<table>"} {print "<tr><td><a href=\"playbooks/"$1".html\">"$1"</a></td><td>"$2"</td></tr>"} END { print "</table>"}'  > output_html/index.html.previous
