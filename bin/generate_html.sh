78d96eb62534d13c0fe5d427475db51c73acdf05
Tangui Morlier <tangui@tangui.eu.org>
2022-01-03 10:51:22 +0100

    Cas des ansible qui échouent

--- bin/generate_html.sh
+++ bin/generate_html.sh
@@ -6,7 +6,9 @@ cd $(dirname $0)/.. 2> /dev/null
 
 mkdir -p output_html/playbooks
 
-grep changed= output_html/playbooks/*html | grep ok= | sed 's/^[^:]*\/\([^:]*\).html:/\1@/' | sed 's/$/<\/p>/' | awk -F '@' 'BEGIN {print "<table>"} {print "<tr><td><a href=\"playbooks/"$1".html\">"$1"</a></td><td>"$2"</td></tr>"} END { print "</table>"}'  > output_html/index.html.tmp
+grep changed= output_html/playbooks/*html | grep ok= > /tmp/ansible_check_generate_html_$$.log
+grep -L 'changed=' output_html/playbooks/*html | sed 's/$/:ERROR/' >> /tmp/ansible_check_generate_html_$$.log
+sort /tmp/ansible_check_generate_html_$$.log | sed 's/^[^:]*\/\([^:]*\).html:/\1@/' | sed 's/$/<\/p>/' | awk -F '@' 'BEGIN {print "<table>"} {print "<tr><td><a href=\"playbooks/"$1".html\">"$1"</a></td><td>"$2"</td></tr>"} END { print "</table>"}'  > output_html/index.html.tmp
 echo -n "<p>Last update: " >> output_html/index.html.tmp ; ls -lrt --full-time  output_html/playbooks/ | tail -n  1 | awk '{print $6" "$7}' >> output_html/index.html.tmp ; echo "</p>" >> output_html/index.html.tmp
 running=$(ls output_html/playbooks/*html.running.htm 2> /dev/null | sed 's/.*playbooks.//' | sed 's/.html.running.htm//')
 if test "$running"; then
@@ -14,6 +16,7 @@ echo "<p><a href='playbooks/"$running".html.running.htm'>"$running" currently ch
 fi
 echo "<p><a href='feed.xml'>RSS Feed</p>" >> output_html/index.html.tmp
 mv output_html/index.html.tmp output_html/index.html
+rm /tmp/ansible_check_generate_html_$$.log
 
 grep changed= output_html/playbooks/*html.previous | grep ok= | sed 's/^[^:]*\/\([^:]*\).html.previous:/\1@/' | sed 's/$/<\/p>/' | awk -F '@' 'BEGIN {print "<table>"} {print "<tr><td><a href=\"playbooks/"$1".html\">"$1"</a></td><td>"$2"</td></tr>"} END { print "</table>"}'  > output_html/index.html.previous
 
