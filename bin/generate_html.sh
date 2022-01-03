71e696f0f8db05e067d376f73238c2ad1044f1f0
Tangui Morlier <tangui@tangui.eu.org>
2022-01-03 15:12:07 +0100

    Un résumé et un titre pour le rapport

--- bin/generate_html.sh
+++ bin/generate_html.sh
@@ -8,15 +8,21 @@ mkdir -p output_html/playbooks
 
 grep changed= output_html/playbooks/*html | grep ok= > /tmp/ansible_check_generate_html_$$.log
 grep -L 'changed=' output_html/playbooks/*html | sed 's/$/:ERROR/' >> /tmp/ansible_check_generate_html_$$.log
-sort /tmp/ansible_check_generate_html_$$.log | sed 's/^[^:]*\/\([^:]*\).html:/\1@/' | sed 's/$/<\/p>/' | awk -F '@' 'BEGIN {print "<table>"} {print "<tr><td><a href=\"playbooks/"$1".html\">"$1"</a></td><td>"$2"</td></tr>"} END { print "</table>"}'  > output_html/index.html.tmp
+sort /tmp/ansible_check_generate_html_$$.log | sed 's/^[^:]*\/\([^:]*\).html:/\1@/' | sed 's/$/<\/p>/' | awk -F '@' 'BEGIN {print "<table>"} {print "<tr><td><a href=\"playbooks/"$1".html\">"$1"</a></td><td>"$2"</td></tr>"} END { print "</table>"}'  > output_html/index.html.table
+rm /tmp/ansible_check_generate_html_$$.log
+
+echo '<h1>Ansible checks</h1>' > output_html/index.html.tmp
+cat output_html/index.html.table | grep '<tr>' | awk -F ':' '{print $2}'  | sed 's/;.*//'  | sort | uniq -c  | awk 'BEGIN {print "<table><tr>"} END {print "</tr></table>"} {print "<td style=\"color:"$2";\">"$1"</td>"}' >> output_html/index.html.tmp
+cat output_html/index.html.table >> output_html/index.html.tmp
+rm output_html/index.html.table
 echo -n "<p>Last update: " >> output_html/index.html.tmp ; ls -lrt --full-time  output_html/playbooks/ | tail -n  1 | awk '{print $6" "$7}' >> output_html/index.html.tmp ; echo "</p>" >> output_html/index.html.tmp
+
 running=$(ls output_html/playbooks/*html.running.htm 2> /dev/null | sed 's/.*playbooks.//' | sed 's/.html.running.htm//')
 if test "$running"; then
 echo "<p><a href='playbooks/"$running".html.running.htm'>"$running" currently checked</a></p>" >> output_html/index.html.tmp
 fi
 echo "<p><a href='feed.xml'>RSS Feed</p>" >> output_html/index.html.tmp
 mv output_html/index.html.tmp output_html/index.html
-rm /tmp/ansible_check_generate_html_$$.log
 
 grep changed= output_html/playbooks/*html.previous | grep ok= | sed 's/^[^:]*\/\([^:]*\).html.previous:/\1@/' | sed 's/$/<\/p>/' | awk -F '@' 'BEGIN {print "<table>"} {print "<tr><td><a href=\"playbooks/"$1".html\">"$1"</a></td><td>"$2"</td></tr>"} END { print "</table>"}'  > output_html/index.html.previous
 
